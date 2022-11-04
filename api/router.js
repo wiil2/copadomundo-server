92940ac40376e2a31a280637cf3c6a29a2ed42ac
William Berbet <wiiliamb@hotmail.com>
2022-11-04 07:29:00 -0300

    mais configs de rotas

--- api/router.js
+++ api/router.js
@@ -1,8 +1,15 @@
 import Router from '@koa/router'
 import * as users from './app/users/index.js'
+import * as hunches from './app/hunches/index.js'
+import * as games from './app/games/index.js'
 
 export const router = new Router()
 
 router.get('/users', users.list)
 router.post('/users', users.create)
+
+//router.get('/users', users.list)
+router.post('/hunches', hunches.create)
+
+router.get('/games', games.list)
 
